f746d3f70f53bd9683f34f7c9e5af810ed46eb89
Alex <olexandrpasalsky@gmail.com>
2021-12-26 15:18:50 +0200

    add Triggers

--- TriggerLab5.sql
+++ TriggerLab5.sql
@@ -18,6 +18,34 @@ SELECT* FROM PharmacyDrugs
 DELETE PharmacyDrugs  where NameDrug = 'Коваміл'
 
 
+				--тригер заміняє дію видалення поля з DirectoryOrder на видалення спочатку звязного поля з ListDrugs і DirectoryOrder
+
+CREATE TRIGGER DeleteDirectoryOrder
+ON DirectoryOrder
+INSTEAD OF DELETE AS
+BEGIN
+	DECLARE @id int 
+	SET @id = (SELECT IdOrder FROM deleted)
+
+
+	DELETE ListDrugs
+	WHERE IdOrder = @id
+	DELETE DirectoryOrder
+	WHERE IdOrder = @id
+END
+
+DROP TRIGGER DeleteNamePharmacyDrugs
+
+Select* FROM DirectoryOrder
+SELECT* FROM ListDrugs
+SELECT* FROM PrescriptionDoctor
+
+DELETE DirectoryOrder where IdOrder = 1;
+
+
+
+
+
 
 
 		--b
@@ -36,6 +64,71 @@ SELECT* FROM PharmacyDrugs
 
 update PharmacyDrugs set [Count] = 1000 where NameDrug = 'Коваміл'
 
+
+				--тригер заміняє апдейт DirectoryOrder коли в DirectoryOrder якомусь замовленню встановлюють стовбцю Bought запису значення 1
+				-- відняти від кількості цього припарату 1 з FactoryDrugs чи PharmacyDrugs
+
+CREATE TRIGGER UpdateDirectoryOrder
+ON DirectoryOrder
+INSTEAD OF UPDATE AS
+BEGIN
+	DECLARE @idOrder int 
+	SET @idOrder = (SELECT IdOrder FROM deleted)
+	DECLARE @namePhDrug nvarchar(50) 
+	SET @namePhDrug = (SELECT NamePharmacyDrug FROM deleted)
+	DECLARE @nameFDrug nvarchar(50) 
+	SET @nameFDrug = (SELECT NameFactoryDrug FROM deleted)
+	DECLARE @bought int
+	SET @bought = (Select Bought FROM deleted)
+
+	Select* FROM deleted
+
+	IF  @bought = 0
+	BEGIN
+		DECLARE @count int
+
+		IF @namePhDrug IS NOT NULL
+		BEGIN
+			SET @count = (SELECT TOP(1) [Count] FROM PharmacyDrugs WHERE NameDrug = @namePhDrug ORDER BY [Count] DESC)
+			SET @count = @count - 1
+
+			UPDATE DirectoryOrder 
+			SET Bought = 1
+			WHERE IdOrder = @idOrder
+
+			UPDATE PharmacyDrugs 
+			SET [Count] = @count
+			WHERE NameDrug = @namePhDrug
+		END
+		
+		IF @nameFDrug IS NOT NULL
+		BEGIN
+			SET @count = (SELECT TOP(1) [Count] FROM FactoryDrugs WHERE NameDrug = @nameFDrug ORDER BY [Count] DESC)
+			SET @count = @count - 1
+		
+
+			UPDATE DirectoryOrder 
+			SET Bought = 1
+			WHERE IdOrder = @idOrder
+
+			UPDATE FactoryDrugs 
+			SET [Count] = @count
+			WHERE NameDrug = @nameFDrug
+		END
+		
+	END
+
+END
+
+DROP TRIGGER UpdateDirectoryOrder
+
+UPDATE DirectoryOrder
+SET Bought = 1
+WHERE IdOrder = 1
+
+SELECT * FROM DirectoryOrder
+SELECT * FROM FactoryDrugs
+Select * FROM PharmacyDrugs
 
 
 		--с
